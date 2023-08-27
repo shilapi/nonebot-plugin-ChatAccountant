009d22ff511d3bb750a99eadf0b37f58c6d45aa4
shilapi <shilapi@outlook.com>
2023-08-27 18:12:18 +0800

    init

---
+++ nonebot_plugin_groupAccountant/__init__.py
@@ -0,0 +1,47 @@
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import Message, MessageEvent
+from nonebot.params import CommandArg
+from nonebot import require
+from nonebot.plugin import PluginMetadata
+from .acountant import writeIn, readBook
+
+require("nonebot_plugin_localstore")
+
+__plugin_meta__ = PluginMetadata(
+    name="群账本",
+    description="记录群友的共同花销，统一清算",
+    usage="1. 记录花销：/记账 [开销名] [人数] [金额]    2. 清算： /清算 [clear](可选，用于清空账本)",
+    extra={
+        "unique_name": "groupAccountant",
+        "example": "/记账 海底捞 5 600",
+        "author": "shilapi <shilapi@outlook.com>",
+        "version": "0.0.1",
+    },
+)
+
+groupAccountant = on_command('accountant', aliases={'记账', '记'}, priority=13, block=True)
+
+@groupAccountant.handle()
+async def _(event: MessageEvent, msg: Message = CommandArg()):
+    if text := msg.extract_plain_text():
+        resp = await writeIn(text, event.get_user_id())
+        #await account.finish('好了'+'(debug:'+str(resp)+str(event.get_user_id())+')')
+        await groupAccountant.finish('好了')
+    else:
+        await groupAccountant.finish('使用方式为：@bot /记账 *开销名称* *人数* *金额*')
+    
+    
+clearance = on_command('reader', aliases={'结账', '清算', '结算'}, priority=13, block=True)
+
+@clearance.handle()
+async def handle_function(event: MessageEvent, msg: Message = CommandArg()):
+    print('start')
+    if text := msg.extract_plain_text():
+        if text == 'final' or 'clear':
+            resp = await readBook(True)
+            await clearance.finish(resp)
+        elif text == 'help':
+            await clearance.finish('使用方式为：@bot /结算 (可选的)clear')
+    else:
+        resp = await readBook(False)
+        await clearance.finish(resp)
---
+++ nonebot_plugin_groupAccountant/acountant.py
@@ -0,0 +1,45 @@
+import json, datetime, os
+from nonebot import require
+
+require("nonebot_plugin_localstore")
+
+import nonebot_plugin_localstore as store
+
+async def writeIn(text: str, userID: str):
+    #main
+    date = str(datetime.datetime.now())[0:19]
+    data = text.split()
+    dataDic = {}
+    innerDataDic = {'location': data[0], 'person': data[1], 'price': data[2], 'by': userID}
+    dataDic[date] = innerDataDic
+    data_dir = store.get_data_dir("accountant")
+    jsonFile = store.get_data_file('accountant', 'accountant book.json')
+    print(jsonFile)
+    if os.path.getsize(jsonFile) == 0:
+        dataJson = json.dumps(dataDic)
+        jsonFile.write_text(dataJson)
+    else:
+        jsonFileData = jsonFile.read_text()
+        dataJson = json.loads(jsonFileData)
+        print(dataJson)
+        dataJson.update(dataDic)
+        dataJson = json.dumps(dataJson)
+        jsonFile.write_text(dataJson)
+        #print(4)
+    return(data_dir)
+
+async def readBook(clear: bool):
+    jsonFile = store.get_data_file('accountant', 'accountant book.json')
+    if os.path.getsize(jsonFile) == 0:
+        resp = '当前无账目信息'
+        return resp
+    jsonFileData = jsonFile.read_text()
+    dataDic = json.loads(jsonFileData)
+    resp = ''
+    for key in dataDic:
+        resp += str(key + '在' + dataDic[key]['location'] + '，' + dataDic[key]['person'] + '人，消费了' + dataDic[key]['price'] + '元，由'+dataDic[key]['by']+'记录\n')
+    if clear == True:
+        jsonFile.write_text('')
+        resp += '账目已最终清算，账本清除'
+    return resp
+        
